ea17a25d85ab052f4b8a4088abe373293d18fa6f
studiokanunu <artist@studiokanunu.com>
2022-04-05 16:41:30 -0700

    ControlZee

--- src/components/qute/index.jsx
+++ src/components/qute/index.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const Qute = ({ data }) => {
     return (
         <div className="slider-content-area" data-aos="fade-up">
-            <h2 dangerouslySetInnerHTML={{ __html: data.qute }}></h2>
+            <h2 dangerouslySetInnerHTML={{ __html: data.qute }}></h2><p class="nice">Collecting art is proven to increase longevity</p>
         </div>
     );
 };
